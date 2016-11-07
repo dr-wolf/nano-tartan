6359cb5515e99cb5b28fbf158a2a78831d47bfcb
Taras "Dr.Wolf" Supyk <w@enigma-lab.org>
2016-11-07 14:43:28 +0200

    Refactor sett parsing

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # nano-tartan
-Extremely small lib for rendering tartans, just for lulz.
+Extremely small lib for rendering tartans in 100 lines of code, just for lulz.
 
 [Demo](https://dr-wolf.github.io/nano-tartan/#A#5c8ca8 AZURE; K#101010 BLACK; B#2c2c80 BLUE; G#006818 GREEN; R#c80000 RED; Y#e8c000 YELLOW;  B/12 R6 B54 G40 Y6 K40 A4 R12 A4 B40 K/14)
--- script.js
+++ script.js
@@ -1,28 +1,4 @@
 (function(){
-  function prepare(sett) {
-    var colors = {
-      B: [0x21, 0x96, 0xF3],
-      G: [0x43, 0xA0, 0x47],
-      K: [0x00, 0x00, 0x00],
-      N: [0x9E, 0x9E, 0x9E],
-      O: [0xFF, 0x98, 0x00],
-      R: [0xF4, 0x43, 0x36],
-      T: [0x79, 0x55, 0x48],
-      W: [0xFF, 0xFF, 0xFF],
-      Y: [0xFF, 0xEB, 0x3B],
-      P: [0x9C, 0x27, 0xB0],
-      I: [0x3F, 0x51, 0xB5]
-    };
-    return {
-      sett:  sett.replace(/([A-Z]+)(#|=|=#)([0-9A-F]{6})[^;]*;/gi, function(m, n, s, c){
-        var color = parseInt('0x' + c);
-        colors[n] = [(color & 0xFF0000) >> 16, (color & 0x00FF00) >> 8, color & 0x0000FF];
-        return '';
-      }),
-      palette: colors
-    };
-  }
-
   function mirror(sett) {
     if (sett.length > 2 && sett[0].match('/') && sett[sett.length - 1].match('/')) {
       sett[0] = sett[0].replace('/', '');
@@ -34,19 +10,27 @@
     return sett;
   }
 
-  function parse({sett, palette}) {
-    var threads = mirror(sett.match(/[A-Z]+\/?[0-9]+/gi)).map(function (t) {
+  function parse(sett) {
+    var palette = {
+      A: [0x0D, 0x47, 0xA1], B: [0x21, 0x96, 0xF3], G: [0x43, 0xA0, 0x47],
+      I: [0x3F, 0x51, 0xB5], K: [0x00, 0x00, 0x00], N: [0x9E, 0x9E, 0x9E],
+      O: [0xFF, 0x98, 0x00], P: [0x9C, 0x27, 0xB0], R: [0xF4, 0x43, 0x36],
+      T: [0x79, 0x55, 0x48], W: [0xFF, 0xFF, 0xFF], Y: [0xFF, 0xEB, 0x3B]
+    };
+    var threads = mirror(sett.replace(/([A-Z]+)(#|=|=#)([0-9A-F]{6})[^;]*;/gi, function(m, n, s, c){
+        var color = parseInt('0x' + c);
+        palette[n] = [(color & 0xFF0000) >> 16, (color & 0x00FF00) >> 8, color & 0x0000FF];
+        return '';
+      }).match(/[A-Z]+\/?[0-9]+/gi)).map(function (t) {
       var [c, w] = t.match(/[A-Z]+|[0-9]+/gi);
       return {
-        color: palette[c] ? palette[c] : palette['W'],
+        color: palette[c] ? palette[c] : palette.W,
         width: parseInt(w)
       };
     });
     return {
       threads: threads,
-      width: threads.reduce(function(w, t){
-        return w + t.width;
-      }, 0)
+      width: threads.reduce(function(w, t){ return w + t.width; }, 0)
     };
   }
 
@@ -58,9 +42,8 @@
     var blob = new Uint8ClampedArray(4 * width * height);
     for (var y = 0; y < height; y++) {
       for (var x = 0; x < width; x++) {
-        var t = colors[sarge(posx + x, posy + y)];
-        blob[(y * width + x) * 4 + 3] = 255;
-        for (var c = 0; c < 3; c++) {
+        var t = colors[sarge(posx + x, posy + y)].concat(255);
+        for (var c = 0; c < 4; c++) {
           blob[(y * width + x) * 4 + c] = t[c];
         }
       }
@@ -108,7 +91,7 @@
     var canvas = document.getElementById("tartan");
     document.getElementById('sett').value = window.location.hash.substr(1) ? decodeURIComponent(window.location.hash.substr(1)) : "B32 Y32";
     var update = function() {
-      render(canvas, parse(prepare(document.getElementById('sett').value)));
+      render(canvas, parse(document.getElementById('sett').value));
     };
     document.getElementById('render').addEventListener('click', update, false);
     window.addEventListener('resize', update);
